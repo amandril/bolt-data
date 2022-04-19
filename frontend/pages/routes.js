89b5eb4f52b3ac7992d49636d6f86760333641fa
alex <alexmandrila@gmail.com>
2022-04-19 08:13:36 -0600

    Index uses bolt count query

--- frontend/pages/routes.js
+++ frontend/pages/routes.js
@@ -8,6 +8,52 @@ const DisplayRoutesStyle = styled.div`
   grid-template-columns: repeat(3, 1fr);
 `;
 
+const BoltGraphStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  div {
+    padding: 5px;
+    text-align: center;
+  }
+`;
+
+const PoorBoltsStyle = styled.div`
+  background-color: red;
+`;
+const AverageBoltsStyle = styled.div`
+  background-color: yellow;
+`;
+const GoodBoltsStyle = styled.div`
+  background-color: green;
+`;
+const BomberBoltsStyle = styled.div`
+  background-color: blue;
+`;
+const UnknownBoltsStyle = styled.div`
+  background-color: gray;
+`;
+
+// Query all the bolts in Colorado
+const ALL_BOLTS_COLORADO = gql`
+  query ALL_BOLTS_COLORADO {
+    poorBolts: _allBoltsMeta(where: { condition: "poor" }) {
+      count
+    }
+    averageBolts: _allBoltsMeta(where: { condition: "average" }) {
+      count
+    }
+    goodBolts: _allBoltsMeta(where: { condition: "good" }) {
+      count
+    }
+    bomberBolts: _allBoltsMeta(where: { condition: "bomber" }) {
+      count
+    }
+    unknownBolts: _allBoltsMeta(where: { condition: "unknown" }) {
+      count
+    }
+  }
+`;
+
 const ALL_ROUTES_QUERY = gql`
   query ALL_ROUTES_QUERY {
     allRoutes(where: { route_name_contains_i: "wrinkle" }) {
@@ -24,10 +70,7 @@ const ALL_ROUTES_QUERY = gql`
 `;
 
 export default function RoutesPage() {
-  const { data, loading, error } = useQuery(ALL_ROUTES_QUERY, {
-    // variables: {
-    // }
-  });
+  const { data, loading, error } = useQuery(ALL_BOLTS_COLORADO);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -35,10 +78,24 @@ export default function RoutesPage() {
   // console.log(data, loading, error);
 
   return (
-    <DisplayRoutesStyle>
-      {data?.allRoutes.map((route) => {
-        return <Route key={route.id} route={route} />;
-      })}
-    </DisplayRoutesStyle>
+    // <DisplayRoutesStyle>
+    //   {data?.allRoutes.map((route) => {
+    //     return <Route key={route.id} route={route} />;
+    //   })}
+    // </DisplayRoutesStyle>
+    <>
+      <div>Bolts and their conditions</div>
+      <BoltGraphStyles>
+        <PoorBoltsStyle>Poor: {data?.poorBolts.count}</PoorBoltsStyle>
+        <AverageBoltsStyle>
+          Average: {data?.averageBolts.count}
+        </AverageBoltsStyle>
+        <GoodBoltsStyle>Good: {data?.goodBolts.count}</GoodBoltsStyle>
+        <BomberBoltsStyle>Bomber: {data?.bomberBolts.count}</BomberBoltsStyle>
+        <UnknownBoltsStyle>
+          Unknown: {data?.unknownBolts.count}
+        </UnknownBoltsStyle>
+      </BoltGraphStyles>
+    </>
   );
 }
